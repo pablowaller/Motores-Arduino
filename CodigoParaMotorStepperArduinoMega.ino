e3f36df9ae8b0d98c919ecaf41e660ab2ebb687a
pablowaller <76492840+pablowaller@users.noreply.github.com>
2023-05-18 16:21:12 -0300

    Update CodigoParaMotorStepperArduinoMega.ino

--- CodigoParaMotorStepperArduinoMega.ino
+++ CodigoParaMotorStepperArduinoMega.ino
@@ -3,37 +3,37 @@ Servo E;
 
 #define X_STEP_PIN 54
 #define X_DIR_PIN 55
-#define X_ENABLE_pin 38
+#define X_ENABLE_PIN 38
 #define X_MIN_PIN 3
-#define X_MAX_PIN -1 //PIN 2 is used
+#define X_MAX_PIN -1 // El PIN 2 se utiliza
 
-void setup(){ 
+void setup() { 
   pinMode(X_DIR_PIN, OUTPUT);
-  pinMode(X_STEP, PIN, OUTPUT);
+  pinMode(X_STEP_PIN, OUTPUT);
   pinMode(X_ENABLE_PIN, OUTPUT);
-  digitalWrite(X_ENABLE_PIN, LOW):
+  digitalWrite(X_ENABLE_PIN, LOW);
 }
 
 void loop() { 
-
   digitalWrite(X_DIR_PIN, HIGH); 
   
-  for(int i=0; i<=3400; i++) {
-    digitalWrite(X_STEP PIN, HIGH); 
+  for (int i = 0; i <= 3400; i++) {
+    digitalWrite(X_STEP_PIN, HIGH); 
     delayMicroseconds(500); 
-    digitalWrite(X_STEP PIN, LOW);
+    digitalWrite(X_STEP_PIN, LOW);
+    delayMicroseconds(500);  // Retardo adicional para asegurar un tiempo mínimo entre pulsos
   } 
   
   delay(1000);
 
-  digltalWrite(X_DIR_PIN, LOW); 
+  digitalWrite(X_DIR_PIN, LOW); 
   
-  for(lnt = 0: <= 3400; i++){
-    digitalWrIte(X_STEP_PIN, HIGH);
-    delay(1);
+  for (int i = 0; i <= 3400; i++) {
+    digitalWrite(X_STEP_PIN, HIGH);
+    delayMicroseconds(500); 
     digitalWrite(X_STEP_PIN, LOW);
+    delayMicroseconds(500);  // Retardo adicional para asegurar un tiempo mínimo entre pulsos
   }
   
   delay(1000);
-
 }
